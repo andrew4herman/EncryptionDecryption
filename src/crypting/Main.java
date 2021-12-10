d8dccefc2c848c6092151cdc844e701de6074d86
Andrii Herman <andrii.herman.w@gmail.com>
2021-12-10 15:56:49 +0200

    Update cryptor initialization

--- src/crypting/Main.java
+++ src/crypting/Main.java
@@ -1,12 +1,13 @@
 package crypting;
 
-import crypting.strategy.*;
+import crypting.strategy.ShiftCipher;
+import crypting.strategy.UnicodeCipher;
 
 public class Main {
 
     public static void main(String[] args) {
         CliParser parser = new CliParser(args);
-        Cryptor cryptor = new Cryptor(new ShiftCipher());
+        Cryptor cryptor;
 
         String outFilePath = parser.optionOf("-out");
         String inFilePath = parser.optionOf("-in");
@@ -15,8 +16,10 @@ public class Main {
         String data = parser.optionOrDefault("-data", "");
         int key = Integer.parseInt(parser.optionOrDefault("-key", "0"));
 
-        if ("unicode".equals(algorithm))
-            cryptor.setCipher(new UnicodeCipher());
+        cryptor = new Cryptor("unicode".equals(algorithm) ?
+                new UnicodeCipher() :
+                new ShiftCipher()
+        );
 
         if (data.isEmpty() && inFilePath != null)
             data = FileReaderWriter.readFrom(parser.optionOf("-in"));
